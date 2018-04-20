4daf87be04c867e22b301012c297ab9523812f51
peterg13 <petergustafson19@yahoo.com>
2018-04-20 15:43:58 -0700

    got SQL and PRAW working

--- SubredditFinder.py
+++ SubredditFinder.py
@@ -1,20 +1,32 @@
+import praw
 import mysql.connector
 from mysql.connector import errorcode
 from auth import *
 
-try:
-	cnx = mysql.connector.connect(user=authUser, password=authPassword, host=authHost, database=authDatabase)
-except mysql.connector.Error as err:
-	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-		print("Something is wrong with your user name or password")
-	elif err.errno == errorcode.ER_BAD_DB_ERROR:
-		print("Database does not exist")
-	else:
-		print(err)
-else:
-	curA = cnx.cursor(buffered=True)
-	query = ("INSERT INTO subreddits VALUES (%s)")
-	data = ('/r/deepfriedmemes',)
-	curA.execute(query, data)
-	cnx.commit()
-	cnx.close()
+reddit = praw.Reddit(client_id='3GkF4Me_oLmFYg',
+                     client_secret='0pElYWgjSUI82canPkHstrkBEik',
+                     user_agent='SubredditFinder')
+
+#subreddit = reddit.random_subreddit(False)
+#print(subreddit.display_name)
+#print(subreddit.public_description)
+#print(subreddit.subscribers)
+#print(subreddit.url)
+#print(subreddit.over18)
+
+#try:
+#	cnx = mysql.connector.connect(user=authuser, password=authpassword, host=authhost, database=authdatabase)
+#except mysql.connector.error as err:
+#	if err.errno == errorcode.er_access_denied_error:
+#		print("something is wrong with your user name or password")
+#	elif err.errno == errorcode.er_bad_db_error:
+#		print("database does not exist")
+#	else:
+#		print(err)
+#else:
+#	cura = cnx.cursor(buffered=true)
+#	query = ("insert into subreddits values (%s)")
+#	data = ('/r/deepfriedmemes',)
+#	cura.execute(query, data)
+#	cnx.commit()
+#	cnx.close()
